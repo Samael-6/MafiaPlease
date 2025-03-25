de020e33850a4e384d487ce3e34766d74334cf65
Samael-6 <gc.yoann@gmail.com>
2025-03-25 18:11:04 +0100

    sound manager

---
+++ Assets/Scripts/SoundManagement.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagement : MonoBehaviour
+{
+
+    public List<AudioSource> SonsJeu;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
